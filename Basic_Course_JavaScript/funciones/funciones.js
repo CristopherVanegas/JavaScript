a81eca787f5b80b043709356d11b2d3258eb198a
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-21 07:22:01 -0500

    CALLED miFuncion(), Then CREATED a expresive or anonimous function and expressed all the instruction to executate, then CALLED the expressive function.

--- Basic_Course_JavaScript/funciones/funciones.js
+++ Basic_Course_JavaScript/funciones/funciones.js
@@ -1,12 +1,15 @@
 
-
 // FUNCIONES DECLARATIVAS -> RESERVA MEMORIA para el calculo o operaciones a hacer en la funci´øn.
 function miFuncion() {
     return 3;
 }
+miFuncion();
 
 
-// FUNCIONES DE EXPRESIÓN
+// FUNCIONES DE EXPRESIÓN O ANONIMAS
+var miSegundaFuncion = function(a, b) {
+    return a + b;
+}
 
-
+miSegundaFuncion(5, 7);
 
