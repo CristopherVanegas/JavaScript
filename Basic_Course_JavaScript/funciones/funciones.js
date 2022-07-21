b0c318a1ea06a8bd676de38cb4af75cc3d4d6cc4
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-21 07:14:27 -0500

    ADDED a declarative function.

---
+++ Basic_Course_JavaScript/funciones/funciones.js
@@ -0,0 +1,12 @@
+
+
+// FUNCIONES DECLARATIVAS -> RESERVA MEMORIA para el calculo o operaciones a hacer en la funci´øn.
+function miFuncion() {
+    return 3;
+}
+
+
+// FUNCIONES DE EXPRESIÓN
+
+
+
