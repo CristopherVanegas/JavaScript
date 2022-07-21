6ad0725fcab2ac54445328cc5b8adfb4cdd6d156
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-21 07:06:23 -0500

    Ways of create a variable.

---
+++ Basic_Course_JavaScript/variables/variables.js
@@ -0,0 +1,16 @@
+var nombre = "Cristopher";
+
+// Dos estados de un variable //
+
+// DECLARADO = RESERVAR MEMORIA para posterior mente dar un valor.
+var edad;
+edad;   // UNDEFINED = ESPACIO RESERVADO sin existir un valor.
+
+// INICIALIZAR = DAR UN VALOR a una variable previamente DECLARADA.
+edad = 30;
+
+
+// ARRAY
+
+var elemento = ["computador", "celular"];
+
