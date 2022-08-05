557bafd5be0c5d18bac9771dde923a7738186215
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-05 06:46:18 -0500

    ADDED A FOLDER AND A SCRIPT ABOUT HOISTING.
    def:
    	Hoisting it's what devs known as the 'declaration' of a variable once it was 'declared and initialized' but instead of x value, this is initialized with 'undefined'.
    
    	example:
    		var x = 'name';
    
    		>>> how js interpretes this is
    		var x = undefined;
    		x = 'name';
    
    	This also happens with 'functions' but in hierarchy this are first declared compared to variables, so it's highly recommended to declare variables and initilize them before using them.

---
+++ Basic_Course_JavaScript/hoisting/hoisting.js
@@ -0,0 +1,13 @@
+
+// Variables y funciones PROCESADAS antes de un código.
+
+console.log(miNombre);
+
+key();
+
+function key() {
+    console.log("Hola " + miNombre);
+}
+
+// var miNombre;   // declarando
+var miNombre = 'Cristopher';    // inicializando
