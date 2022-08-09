74592a17ae16b49703aec3461e6197e3f6d49249
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-08 23:33:29 -0500

    OPERATORS

---
+++ Basic_Course_JavaScript/Operadores_Asignación_Comparación_Aritméticos/operadores.js
@@ -0,0 +1,36 @@
+
+
+/*  ARITMÉTICOS  -  BINARY OPERATORS  */
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+
+
+/*  UNARY OPERATORS -> OPERADORES UNITARIOS  */
+!false;
+
+
+/*   COMPARATIVE OPERATORS   */
+3 == "3";   // True   ->    COMPARES JUST THE VALUE BUT NOT THE TYPE, THAT'S WHY IT'S TRUE.
+3 === "3";  // False  ->    COMPARES THE VALUE AND TYPE, THAT'S WHY IT'S FALSE.
+
+
+/*  DIFFERENCES  */
+5 < 3
+5 > 3
+5 <= 6
+5 >= 6
+
+
+/*  BINARY OPERATORS && and ||  */
+a && b
+a || b
+
+
+/*  INCREMENTAL OPERATORS  */
+var edad = 40
+edad ++
+
+edad += 2
+
