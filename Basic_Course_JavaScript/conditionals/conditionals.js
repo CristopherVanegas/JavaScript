2dcc75b39f0232514026c8af724d20383e2b2cb2
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-09 00:40:13 -0500

    ADDED CONDITIONALS AND PRIMARY CONDITION.

---
+++ Basic_Course_JavaScript/conditionals/conditionals.js
@@ -0,0 +1,21 @@
+
+var edad = 18;
+
+if (edad  === 18) {
+    console.log('Puedes votar, será tu primera votación');
+} else if (edad > 18) {
+    console.log('Puedes votar de nuevo');
+} else {
+    console.log('Aún no puedes votar');
+}
+
+
+
+/*  PRIMARY OPERATOR condition ? true : false;  */
+
+var numero = 2;
+
+var resultado = numero === 1 ? 'Si soy un uno' : 'No, soy un uno';  // If and Else just in one-line.
+
+console.log(resultado);
+
