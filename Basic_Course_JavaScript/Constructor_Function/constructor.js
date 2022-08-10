faa356e410052d219253f4490833332b47d5ec7e
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-10 13:35:27 -0500

    ADDED Comments

--- Basic_Course_JavaScript/Constructor_Function/constructor.js
+++ Basic_Course_JavaScript/Constructor_Function/constructor.js
@@ -1,11 +1,14 @@
 
-function auto (marca, modelo, annio) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.annio = annio;
+/*   CONSTRUCTOR FUNCTION   */
+
+function auto (marca, modelo, annio) {  // As parameters it receives (marca, model and annio of release)
+    this.marca = marca; // From this object 'marca', It's equal to parameter 'marca'.
+    this.modelo = modelo;   // From this object 'modelo', It's equal to parameter 'modelo'.
+    this.annio = annio; // From this object 'annio', It's equal to parameter 'annio'.
 }
 
-var autoNuevo = new auto("Tesla", "Model 3", 2020);
-var autoNuevo2 = new auto("Ford", "Maverick", 1975);
+var autoNuevo1 = new auto("Tesla", "Model 3", 2020);    // Instance of object 'auto' for Tesla Model 3
+var autoNuevo2 = new auto("Ford", "Maverick", 1975);    // Instance of object 'auto' for Ford Maverick
 
-// console.log(autoNuevo);
+console.log(autoNuevo1);    // prints by console the first car
+console.log(autoNuevo2);    // prints by console the second car
