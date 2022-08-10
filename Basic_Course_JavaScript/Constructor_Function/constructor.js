56e4594b61ffc2e2c97aac7b960159401e24e1ea
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-10 11:13:13 -0500

    CONSTRUCTOR FUNCTION

---
+++ Basic_Course_JavaScript/Constructor_Function/constructor.js
@@ -0,0 +1,11 @@
+
+function auto (marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Tesla", "Model 3", 2020);
+var autoNuevo2 = new auto("Ford", "Maverick", 1975);
+
+// console.log(autoNuevo);
