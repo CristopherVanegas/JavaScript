9527d864c40c8519ff6a37b3f512fd1ae28a3bc9
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-09 23:33:17 -0500

    For... of

--- Basic_Course_JavaScript/for_and_forOf/for_and_forOf.js
+++ Basic_Course_JavaScript/for_and_forOf/for_and_forOf.js
@@ -10,7 +10,6 @@ function saludarEstudiantes(estudiante) {
     console.log(`Hola, ${estudiante}`);
 }
 
-for (var index = 0; index < estudiantes.length; index ++) {
-    saludarEstudiantes(estudiantes[index]);
+for (var estudiante of estudiantes) {    // estudiante -> index
+    saludarEstudiantes(estudiante);
 }
-
