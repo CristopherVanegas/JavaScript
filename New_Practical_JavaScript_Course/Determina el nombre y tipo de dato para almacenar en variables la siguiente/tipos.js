15de95aa4cd7c324305f280430768ecc0df4a294
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-17 22:00:31 -0500

    ADDED EXERCISE 2

--- New_Practical_JavaScript_Course/Determina el nombre y tipo de dato para almacenar en variables la siguiente/tipos.js
+++ New_Practical_JavaScript_Course/Determina el nombre y tipo de dato para almacenar en variables la siguiente/tipos.js
@@ -5,5 +5,10 @@ let Edad = 18;
 let correoElectrónico = "myemail@gmail.com";
 let mayorDeEdad = true;
 let dineroAhorrado = 10;
-let deudas = false;
+let deudas = 2;
 
+let nombreCompleto = nombre + " " + apellido;
+let dineroReal = dineroAhorrado - deudas;
+
+console.log(nombreCompleto);
+console.log(dineroReal);
