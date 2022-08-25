d343b3265a4c8ce9dd5ecd97ef444db664723a51
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-24 20:32:12 -0500

    SELECTED ELEMENTS

--- New_Practical_JavaScript_Course/DOM_Handling/script.js
+++ New_Practical_JavaScript_Course/DOM_Handling/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
