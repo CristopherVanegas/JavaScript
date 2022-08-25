e5d9635c4e1907ca275d348160f52e9a44cdc38a
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-25 18:13:30 -0500

    GETTING, SETTING AND DELETING ATTRIBUTED

--- New_Practical_JavaScript_Course/DOM_Handling/script.js
+++ New_Practical_JavaScript_Course/DOM_Handling/script.js
@@ -17,11 +17,28 @@ console.log(h1);
 console.log(input.value);
 console.log(input.value = 123);
 
-console.log(p.innerHTML = "Modifiqué el InnerHTML");
 
+h1.innerHTML = 'Patito <br> Feo'
+h1.innerText = 'Patito <br> Feo'
+console.log(p.innerHTML = "Modifiqué el InnerHTML");
 console.log(p);
 
 const parrafos = document.querySelectorAll('p');
 console.log( "Lista de Parrafos: ");
 console.log(parrafos);
 
+
+
+// GET THE CLASS ATTRIBUTE
+attribute = h1.getAttribute('class');
+console.log("USING .getAttribute()")
+console.log("El atributo de tipo 'clase' del elemento h1 es: " +  "." + attribute);
+
+
+// MODIFY THE ATTRIBUTE
+h1.setAttribute('class', 'rojo');
+h1.classList.add('verde');
+
+//h1.classList.remove('verde');
+
+console.log(h1);
