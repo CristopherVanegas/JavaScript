544d50b9badd14a08c35332c6329461b66640688
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-25 18:52:31 -0500

    FNISHED DOM HANDLING FUNDAMENTAL CLASS
    1. REORGANIZED CODE
    2. CREATED IMG ELEMENT AND APPENDED INTO A pid or p element.

--- New_Practical_JavaScript_Course/DOM_Handling/script.js
+++ New_Practical_JavaScript_Course/DOM_Handling/script.js
@@ -1,7 +1,8 @@
+/* SELECTING AND ADDING INTO VARIABLES THE QUERYSELECTORS AND IT'S ELEMENTS */
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log({
@@ -11,11 +12,11 @@ console.log({
     pid,
     input
 });
-
 console.log(h1);
 
-console.log(input.value);
-console.log(input.value = 123);
+
+/* INPUT */
+console.log(input.value);   //console.log(input.value = 123);
 
 
 h1.innerHTML = 'Patito <br> Feo'
@@ -23,22 +24,43 @@ h1.innerText = 'Patito <br> Feo'
 console.log(p.innerHTML = "Modifiqué el InnerHTML");
 console.log(p);
 
+
 const parrafos = document.querySelectorAll('p');
 console.log( "Lista de Parrafos: ");
 console.log(parrafos);
 
 
 
-// GET THE CLASS ATTRIBUTE
+/* GET THE CLASS ATTRIBUTE */
 attribute = h1.getAttribute('class');
 console.log("USING .getAttribute()")
 console.log("El atributo de tipo 'clase' del elemento h1 es: " +  "." + attribute);
 
 
-// MODIFY THE ATTRIBUTE
+/* MODIFY THE ATTRIBUTE */
 h1.setAttribute('class', 'rojo');
 h1.classList.add('verde');
-
 //h1.classList.remove('verde');
-
 console.log(h1);
+
+
+
+/* MODIFY INPUT VALUES */
+input.value = 1234567;
+
+
+
+/* CREATE A NEW ELEMENT */
+const img = document.createElement('img');
+img.setAttribute('src', 'https://cdn-icons-png.flaticon.com/512/919/919826.png');
+
+const img_2 = document.createElement('img');
+img_2.setAttribute('src', 'https://wwwhatsnew.com/wp-content/uploads/2021/01/aprender-css.jpg');
+
+console.log(img_2);
+
+//pid.innerHTML
+//pid.appendChild
+pid.append(img);
+p.innerHTML = "";
+p.appendChild(img_2);
