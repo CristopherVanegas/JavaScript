153e96df244824bbb1033535da96c94fad62e52f
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-24 20:51:03 -0500

    CHANGES
    PRINTED a object of HTML objects.
    MODIFIED input.value.
    MODIFIED p.innerHTML.
    USED querySelectorAll for 'p' HTML label.

--- New_Practical_JavaScript_Course/DOM_Handling/script.js
+++ New_Practical_JavaScript_Course/DOM_Handling/script.js
@@ -4,3 +4,24 @@ const parrafito = document.querySelector('.parrafito');
 const pid = document.querySelector('#pid');
 const input = document.querySelector('input');
 
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
+
+console.log(h1);
+
+console.log(input.value);
+console.log(input.value = 123);
+
+console.log(p.innerHTML = "Modifiqué el InnerHTML");
+
+console.log(p);
+
+const parrafos = document.querySelectorAll('p');
+console.log( "Lista de Parrafos: ");
+console.log(parrafos);
+
