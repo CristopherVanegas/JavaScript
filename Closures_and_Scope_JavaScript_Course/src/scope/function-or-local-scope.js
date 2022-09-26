fb10e9d2be661d4540b50431cd7bde317137b370
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-25 20:21:50 -0500

    ADDED Local Scope

---
+++ Closures_and_Scope_JavaScript_Course/src/scope/function-or-local-scope.js
@@ -0,0 +1,12 @@
+//  local scope
+function greeting() {
+    let userName = 'Ana';
+    console.log(userName);
+
+    if (userName === 'Ana') {
+        console.log(`Hello ${userName}!`);
+    }
+}
+
+greeting();
+console.log(userName);  // Not defined
