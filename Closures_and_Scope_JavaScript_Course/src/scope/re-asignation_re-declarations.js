f468b61cde92bb07ae7fd01be417ef49283c8a41
Andy Apolinario <layber200309@gmail.com>
2022-09-29 07:25:24 -0500

    ADDED re-asignation_re-declarations.js for variables and consts

---
+++ Closures_and_Scope_JavaScript_Course/src/scope/re-asignation_re-declarations.js
@@ -0,0 +1,17 @@
+let fruit = 'Banana';
+console.log(fruit);
+
+// const
+const animal = 'dog';   // declaration and asignation
+// animal = 'cat';  // reasignation
+
+// const animal = 'Snake';
+console.log(animal);
+
+
+const vehicles = [];
+vehicles.push("A");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
