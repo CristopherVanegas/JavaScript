fd96c12d0585ba2b0e80d462e7e3677e945b6736
CristopherVanegas <cristopher.platzi@gmail.com>
2022-10-07 23:46:01 -0500

    ADDED Closure exercise

---
+++ Closures_and_Scope_JavaScript_Course/src/closure/myMoneyBox.js
@@ -0,0 +1,16 @@
+// create a money box using CLOSURES
+
+function moneyBox() {
+    let saveCoins = 0;
+    function countCoins(coins) {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+
+myMoneyBox(5);
+myMoneyBox(19);
+myMoneyBox(20);
