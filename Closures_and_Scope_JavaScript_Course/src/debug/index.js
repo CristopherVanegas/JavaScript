6e9aac7a1fa5f5cd9707aa3f04579f4735f95f21
Cristopher Vanegas <cristopher.platzi@gmail.com>
2022-10-23 19:19:03 -0500

    DEBUGGER for browser

---
+++ Closures_and_Scope_JavaScript_Course/src/debug/index.js
@@ -0,0 +1,9 @@
+var a = 'hello';
+
+function hello() {
+    debugger;   // Opens the debugger and pause the running in the browser.
+    let b = 'Hello World';
+    const c = 'Hello World';
+}
+
+hello();
